e04117f3d2dacdcb45d130c18e9673263478041c
ap0424ts <ap0424ts@icloud.com>
2022-11-04 16:29:10 +0900

    ActiveStorageの導入

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -4,4 +4,10 @@ class Public::ItemsController < ApplicationController
 
   def show
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :content, :material, :centimeter, :price, :reservation_id, :size_id, :images).merge(user_id: admin_user.id)
+  end
 end
