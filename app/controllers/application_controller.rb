5c05272ca27503d894fd128525a20fa0fc0c9e02
ap0424ts <ap0424ts@icloud.com>
2022-11-08 18:19:22 +0900

    不具合の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,8 @@ class ApplicationController < ActionController::Base
   
   def current_cart
     if current_user
-      # ユーザーとカートの紐付け
       current_cart = current_user.cart || current_user.create_cart!
     else
-      # セッションとカートの紐付け
       current_cart = Cart.find_by(id: session[:cart_id]) || Cart.create
       session[:cart_id] ||= current_cart.id
     end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,3 @@
 class Cart < ApplicationRecord
   has_many :cart_items, dependent: :destroy
-  has_many :items, through: :cart_items
-  belongs_to :user
 end
