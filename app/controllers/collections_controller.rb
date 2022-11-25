98793da0538cf13e3a1a3161879290e72f34bb3d
ap0424ts <ap0424ts@icloud.com>
2022-11-25 17:15:42 +0900

    画像投稿機能の実装

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,4 +1,24 @@
 class CollectionsController < ApplicationController
+
+  def index
+    @collections = Collection.all.order("created_at DESC")
+  end
+
+  def new
+    @collection = Collection.new
+  end
+
+  def create
+    @collection = Collection.new(collection_params)
+    if @collection.save
+      redirect_to collections_path
+    else
+      render :new
+    end
+  end
+
+
+  private
   def collection_params
     params.require(:collection).permit(:season, images:[])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,6 @@ Rails.application.routes.draw do
     end
   end
 
+  resources :collections, only: [:index, :new, :create, :edit ,:destroy]
+
 end
